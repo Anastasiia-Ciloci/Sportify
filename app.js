fe03ae2c7c07489524e8e00e4b2a96d26e2a9b8a
Seth Wooten <slwooten14@gmail.com>
2022-04-06 18:22:35 -0400

    added relationships and models in models folder, added seeds folder, nothing in it yet

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // MIDDLEWARES
 
---
+++ models/Intensity.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Intensity extends Model {}
+
+Intensity.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    level: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Intensity;
