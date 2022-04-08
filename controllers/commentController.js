c61928c1c311eab01e2588b8b6f10528254708b1
janinquisitor <jancarlos0195@gmail.com>
2022-04-08 13:49:58 -0400

    Added global error handler and some authentication

---
+++ controllers/commentController.js
@@ -0,0 +1,23 @@
+const AppError = require('../utils/appError')
+const Comment = require('../models/Comment');
+const catchAsync = require('../utils/catchAsync')
+
+exports.createComment = catchAsync(async (req, res, next) => {
+
+});
+
+exports.getAllComments = catchAsync(async (req, res, next) => {
+
+});
+
+exports.getComment = catchAsync(async (req, res, next) => {
+
+});
+
+exports.updateComment = catchAsync(async (req, res, next) => {
+
+});
+
+exports.deleteComment = catchAsync(async (req, res, next) => {
+
+});
