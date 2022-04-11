f5fc61b607e2aba25863b756db15916fadc85c22
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-04-11 10:33:11 -0400

    succesfully rendered home page

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 // PACKAGE IMPORTS
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const hpp = require("hpp");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 // ROUTES IMPORTS
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/userRoutes');
-const workoutRouter = require('./routes/workoutRoutes');
-const intensityRouter = require('./routes/intensityRoutes');
-const commentRouter = require('./routes/commentRoutes');
-const viewsRouter = require('./routes/viewsRoutes');
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/userRoutes");
+const workoutRouter = require("./routes/workoutRoutes");
+const intensityRouter = require("./routes/intensityRoutes");
+const commentRouter = require("./routes/commentRoutes");
+const viewsRouter = require("./routes/viewsRoutes");
 
 // EXPRESS
 const app = express();
 
 // Serve public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // MIDDLEWARES
 
@@ -39,8 +39,8 @@ app.set('view engine', 'handlebars');
 if (process.env.NODE_ENV === "development") app.use(logger("dev"));
 
 // Body parser
-app.use(express.json({limit: "10kb"}));
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set security HTTP Headers.
@@ -50,17 +50,16 @@ app.use(helmet());
 app.use(xss());
 
 // ROUTES
-app.use('/api/v1/user', userRouter);
-app.use('/api/v1/workout', workoutRouter);
-app.use('/api/v1/intensity', intensityRouter)
-app.use('/api/v1/comment', commentRouter)
-app.use('/', viewsRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/workout", workoutRouter);
+app.use("/api/v1/intensity", intensityRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/", viewsRouter);
 // Will probably add an index api file in the future and nest all the API routes inside.s
-
 
 // ERROR HANDLERS
 app.all("*", (req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404)); // <--- When this catches an error.
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404)); // <--- When this catches an error.
 });
 
 // Error handler
--- models/Workout.js
+++ models/Workout.js
@@ -1,66 +1,67 @@
-const {Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 const slugify = require("slugify");
-const sequelize = require('../config/connection');
-const catchAsync = require('../utils/catchAsync');
+const sequelize = require("../config/connection");
+const catchAsync = require("../utils/catchAsync");
 
-const intensityLevels = ['Easy', 'Medium', 'Hard'];
+const intensityLevels = ["Easy", "Medium", "Hard"];
 
-class Workout extends Model {
-}
+class Workout extends Model {}
 
 Workout.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.STRING,
-        }, intensity: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                checkIntensity(value) {
-                    if (!intensityLevels.includes(value)) throw new Error('Workout must have an intensity level.')
-                }
-            }
-        },
-        time_frame: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        slug: {
-            type: DataTypes.STRING,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        hooks: {
-            beforeCreate: catchAsync(async (workout) => {
-                // console.log(workout.title)
-                // Can also access workout object values as: "workout.title"
-                const data = workout.dataValues;
-                workout.dataValues.slug = await slugify(data.title, {lower: true});
-            })
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'workout',
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+    },
+    /*intensity: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        checkIntensity(value) {
+          if (!intensityLevels.includes(value))
+            throw new Error("Workout must have an intensity level.");
+        },
+      },
+    },*/
+    time_frame: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    slug: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: catchAsync(async (workout) => {
+        // console.log(workout.title)
+        // Can also access workout object values as: "workout.title"
+        const data = workout.dataValues;
+        workout.dataValues.slug = await slugify(data.title, { lower: true });
+      }),
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "workout",
+  }
 );
 
 module.exports = Workout;
